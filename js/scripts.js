7bf3fd4c188c64790cbc2d5d964bc49c6a57f71d
EunC02 <147002435+EunC02@users.noreply.github.com>
2025-09-05 21:51:48 +0900

    modified sizes and locations
    size and loc modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
 console.log("Hi! Welcome to my Portfolio");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$(document).ready(function(){
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 800);
+        }
+      }
+    });
+});
+
